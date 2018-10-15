4ad971945a9ad02d3e9c21cfb7fe9e95773a70bd
Rafael <rafaeljhs50@gmail.com>
2018-10-15 10:41:33 -0300

    apoiadores

---
+++ Scripts/C_Logar.js
@@ -0,0 +1,40 @@
+﻿function Enviar() {
+    if ($('#txtEmail').val().length > 0 && $('#txtSenha').val().length > 0) {
+        $("#divloading").toggle();
+    }
+}
+
+function VerificaEmail() {
+    if ($('#txtEmail').val() == "") {
+        bootbox.alert('<div class=\"alert alert-danger text-center\" role=\"alert\"><strong>Informe o email !</strong></div>');
+    return false;
+    }
+    return true
+}
+
+function RecuperarSenha() {
+    if (VerificaEmail())
+    {
+        $("#divloading").toggle();
+        $.ajax({
+            type: 'POST',
+            url: '/Home/RecuperarSenha',
+            data: { Email: $('#txtEmail').val() },
+            dataType: 'json',
+            success: function (retorno) {
+                $("#divloading").toggle();
+                if (retorno > 0) {
+                    bootbox.alert('<div class=\"alert alert-danger text-center\" role=\"alert\"><strong>&Oacutetimo , e-mail para redefinir senha enviado !</strong></div>');
+                }
+                else if (retorno == -1007) {
+                    bootbox.alert('<div class=\"alert alert-danger text-center\" role=\"alert\"><strong>Usu&aacuterio n&atildeo encontrado</strong></div>');
+                }
+            },
+            error: function (erro) {
+                $("#divloading").toggle();
+                bootbox.alert('<div class="alert alert-danger"><strong>Falha ao enviar!</strong></div>');
+            }
+        });
+    }
+}
+
